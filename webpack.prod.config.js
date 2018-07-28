b571379b1d6459edfbacaad7baad9406080bedd7
NancyChan <fanglanchan@gmail.com>
2018-07-29 02:05:39 +1200

    update webpack prod

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -11,6 +11,7 @@ module.exports = {
     path: path.join(__dirname, '/dist/')
 	},
   entry: './src/index.tsx',
+  devtool: 'source-map',
 
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.json'],
