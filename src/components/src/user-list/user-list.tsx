3480adf855b6b4b95b2043aec8e936cc61164893
NancyChen <fanglanchans@gmail.com>
2018-10-02 08:48:21 +1300

    HOC loader

--- src/components/src/user-list/user-list.tsx
+++ src/components/src/user-list/user-list.tsx
@@ -4,13 +4,15 @@ import { UserListGrid } from './user-list-grid';
 import { UserListFilter } from './user-list-filter';
 import { UserStore } from '../../../stores/src/user-store';
 import './scss/user-list.scss';
+import { WrapComponentWithLoader } from '../with-loader/with-loader';
+import { UserListSkeleton } from './user-list-skeleton';
 
 export interface UserListProps {
     userStore: UserStore;
 }
 
 @observer
-export class UserList extends React.Component<UserListProps> {
+export class UserListInternal extends React.Component<UserListProps> {
     render() {
         return (
             <section className="p-main">
@@ -24,3 +26,11 @@ export class UserList extends React.Component<UserListProps> {
         await this.props.userStore.fetchUsers();
     }
 }
+
+export const UserList = (props: UserListProps) => {
+    // has data logic TODO ...
+    
+    const hasData = true;
+    const UserListWithLoader = WrapComponentWithLoader<UserListProps, {}>(UserListSkeleton, hasData)(UserListInternal);
+    return (<UserListWithLoader {...props} />);
+};
